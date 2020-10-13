3bd504ce29f76c6ec9b6f7992f3f6cc23cd7585b
Sami Alfattani <sami_alfattani@hotmai.com>
2020-10-13 14:55:33 +0300

    fix navigation problem

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) 
+  {
+    return 
+      new Scaffold(
+          appBar: new AppBar(
+                title: Text('Main Page'),
+           ),
+          body: new RaisedButton(
+            onPressed: () => Navigator.pushNamed(context, '/MyList' ), 
+            child: new Text('List view')
+            )
+      );
+
+  }//end build
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,23 @@
 
 import 'package:flutter/material.dart';
-
+import 'list_with_stateless.dart';
+import 'HomeScreen.dart';
 void main() =>  runApp(MyApp());
 
 class MyApp extends StatelessWidget 
 {
-  List<Widget> lst = [];
-  MyApp()
-  {
-    for (var i = 0; i < 20; i++){
-      var l = ListTile(
-        title: Text('item $i'),
-        onTap: () => print('click item $i'), );
-      
-      lst.add(l);
-    }
-  }
-
-  int x = 0;
   @override
   Widget build(BuildContext context) 
   {
     
-    return new MaterialApp(
-     home: Scaffold(
-          appBar: new AppBar(
-                    title: Text('ddddd'),
-                    actions: [
-                        IconButton(icon:  new Icon( Icons.account_balance_wallet ), 
-                              onPressed: myAction_On_appbar )
-                      ],
-           ),
-          body: ListView(children:lst,),
-      )
+    return 
+    new MaterialApp(
+      home: new HomeScreen(),
+      
+      routes: <String, WidgetBuilder>{
+        '/MyList': (BuildContext context) => new MyList(),
+      },
     ); 
   } // Widget
-
-  void myAction_On_appbar(){
-    print( "hllllllllllllo " + (this.x+1).toString() );
-  }
 
 } //StatelessWidget 
