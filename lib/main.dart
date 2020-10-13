dbdcebba81e2208a17638cf2c5dc2496a66d8251
Sami Alfattani <sami_alfattani@hotmai.com>
2020-10-13 11:31:19 +0300

    first push

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+
+void main() =>  runApp(MyApp());
+
+class MyApp extends StatelessWidget 
+{
+  List<Widget> lst = [];
+  MyApp()
+  {
+    for (var i = 0; i < 20; i++){
+      var l = ListTile(
+        title: Text('item $i'),
+        onTap: () => print('click item $i'), );
+      
+      lst.add(l);
+    }
+  }
+
+  int x = 0;
+  @override
+  Widget build(BuildContext context) 
+  {
+    
+    return new MaterialApp(
+     home: Scaffold(
+          appBar: new AppBar(
+                    title: Text('ddddd'),
+                    actions: [
+                        IconButton(icon:  new Icon( Icons.account_balance_wallet ), 
+                              onPressed: myAction_On_appbar )
+                      ],
+           ),
+          body: ListView(children:lst,),
+      )
+    ); 
+  } // Widget
+
+  void myAction_On_appbar(){
+    print( "hllllllllllllo " + (this.x+1).toString() );
+  }
+
+} //StatelessWidget 
